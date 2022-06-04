f826d821efd50f02c906eaa6f12b6b9884c547be
Brianna Gaines <briannagaines31@gmail.com>
2022-06-04 14:53:33 -0600

    finalizes backend

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express()
 const api_helper = require('./API_helper')
 
 const mongoose = require("mongoose")
-const request = require('request');
 const cors = require("cors")
 const morgan = require("morgan")
 
@@ -49,17 +48,7 @@ app.get("/meals/new", async (req,res)=>{
   }
 })
 
-// app.get("/meals/recipes", async (req,res)=>{
-//   try{
-//      res.send("recipes")
-//   } catch (error) {
-//       res.status(400).json(error)
-//   }
-// })
-https://api.spoonacular.com/recipes/random?number=50&apiKey=e61e9a21648e4ecea611937d92a2c146
 app.get('/meals/recipes', async (req, res) => {
-  res.json(req.body)
-  console.log(req.body)
   api_helper.make_API_call('https://api.spoonacular.com/recipes/random?number=50&apiKey=e61e9a21648e4ecea611937d92a2c146')
   .then(response => {
       res.json(response)
