4444f6815877a58d0a6f3409226c08fdcc853fa1
Brianna Gaines <briannagaines31@gmail.com>
2022-06-04 12:57:43 -0600

    updating backend for api call

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ require("dotenv").config()
 const { PORT = 3001, DATABASE_URL } = process.env
 const express = require("express")
 const app = express()
+const api_helper = require('./API_helper')
 
 const mongoose = require("mongoose")
-
+const request = require('request');
 const cors = require("cors")
 const morgan = require("morgan")
 
@@ -48,13 +49,26 @@ app.get("/meals/new", async (req,res)=>{
   }
 })
 
-app.get("/meals/recipes", async (req,res)=>{
-  try{
-     res.send("recipes")
-  } catch (error) {
-      res.status(400).json(error)
-  }
+// app.get("/meals/recipes", async (req,res)=>{
+//   try{
+//      res.send("recipes")
+//   } catch (error) {
+//       res.status(400).json(error)
+//   }
+// })
+https://api.spoonacular.com/recipes/random?number=50&apiKey=e61e9a21648e4ecea611937d92a2c146
+app.get('/meals/recipes', async (req, res) => {
+  res.json(req.body)
+  console.log(req.body)
+  api_helper.make_API_call('https://api.spoonacular.com/recipes/random?number=50&apiKey=e61e9a21648e4ecea611937d92a2c146')
+  .then(response => {
+      res.json(response)
+  })
+  .catch(error => {
+      res.send(error)
+  })
 })
+
 
 app.post("/meals", async (req, res) => {
     try {
